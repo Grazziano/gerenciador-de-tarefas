ce7d0bdbd115bcb161ea78f22969fd6cc1548be1
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-16 18:17:18 -0300

    Criando o formulário de cadastro de tarefas

--- src/components/CadastrarTarefa/CadastrarTarefa.js
+++ src/components/CadastrarTarefa/CadastrarTarefa.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Alert, Button, Form, Modal } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function CadastrarTarefa() {
   return (
     <div>
-      <h1>Cadastrar Tarefas</h1>
+      <h3 className="text-center">Cadastrar Tarefa</h3>
+      <Alert variant="secondary">
+        <Form>
+          <Form.Group>
+            <Form.Label>Tarefa</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Digite a tarefa"
+              minLength="5"
+              maxLength="100"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              A tarefa deve conter ao menos 5 caracteres.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="text-center mt-2">
+            <Button variant="success" type="submit">
+              Cadastrar
+            </Button>
+            &nbsp;
+            <Link to="/" className="btn btn-light">
+              Voltar
+            </Link>
+          </Form.Group>
+        </Form>
+      </Alert>
     </div>
   );
 }
