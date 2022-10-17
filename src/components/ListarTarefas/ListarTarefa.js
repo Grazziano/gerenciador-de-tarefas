416d97c7446d84b943ceb9618c846a9f1833637b
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-17 18:11:34 -0300

    Criando  html da listagem de tarefas

--- src/components/ListarTarefas/ListarTarefa.js
+++ src/components/ListarTarefas/ListarTarefa.js
@@ -1,13 +1,31 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function ListarTarefas() {
   return (
-    <div>
-      <h1>Listagem de Tarefas</h1>
-      <Link to="/cadastrar" className="btn btn-success btn-sm">
-        Nova Tarefa
-      </Link>
+    <div className="text-center">
+      <h3>Tarefas a fazer</h3>
+      <Table striped bordered hover responsive data-testid="tabela">
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>
+              <Link
+                to="/cadastrar"
+                className="btn btn-success btn-sm"
+                data-testid="btn-nova-tarefa"
+              >
+                <FontAwesomeIcon icon={faPlus} /> Nova tarefa
+              </Link>
+            </th>
+          </tr>
+        </thead>
+
+        <tbody></tbody>
+      </Table>
     </div>
   );
 }
