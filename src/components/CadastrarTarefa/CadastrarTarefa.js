9278ec0fdf7eba906c2169600ecab22f8ea9ec52
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-17 17:06:03 -0300

    Implementando a ação de cadastro de tarefas

--- src/components/CadastrarTarefa/CadastrarTarefa.js
+++ src/components/CadastrarTarefa/CadastrarTarefa.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Alert, Button, Form, Modal } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function CadastrarTarefa() {
+  const [tarefa, setTarefa] = useState('');
+  const [formValidado, setFormValidado] = useState(false);
+  const [exibirModal, setExibirModal] = useState(false);
+
+  const navigate = useNavigate();
+
+  function cadastrar(event) {}
+
+  function handleTxtTarefa(event) {
+    setTarefa(event.target.value);
+  }
+
+  function handleFecharModal() {
+    navigate('/');
+  }
+
   return (
     <div>
       <h3 className="text-center">Cadastrar Tarefa</h3>
       <Alert variant="secondary">
-        <Form>
+        <Form validated={formValidado} noValidate onSubmit={cadastrar}>
           <Form.Group>
             <Form.Label>Tarefa</Form.Label>
             <Form.Control
@@ -16,6 +32,8 @@ function CadastrarTarefa() {
               minLength="5"
               maxLength="100"
               required
+              value={tarefa}
+              onChange={handleTxtTarefa}
             />
             <Form.Control.Feedback type="invalid">
               A tarefa deve conter ao menos 5 caracteres.
@@ -31,13 +49,15 @@ function CadastrarTarefa() {
             </Link>
           </Form.Group>
         </Form>
-        <Modal show={false}>
+        <Modal show={exibirModal} onHide={handleFecharModal}>
           <Modal.Header closeButton>
             <Modal.Title>Sucesso</Modal.Title>
           </Modal.Header>
           <Modal.Body>Tarefa adicionada com sucesso!</Modal.Body>
           <Modal.Footer>
-            <Button variant="success">Continuar</Button>
+            <Button variant="success" onClick={handleFecharModal}>
+              Continuar
+            </Button>
           </Modal.Footer>
         </Modal>
       </Alert>
