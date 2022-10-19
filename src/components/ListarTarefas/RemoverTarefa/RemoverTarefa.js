6848df7e1f0f4dc1a23fefe41d4f2e2c0e57dc87
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-19 18:49:01 -0300

    Criando html para remover uma tarefa

--- src/components/ListarTarefas/RemoverTarefa/RemoverTarefa.js
+++ src/components/ListarTarefas/RemoverTarefa/RemoverTarefa.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Button } from 'react-bootstrap';
 
 function RemoverTarefa(props) {
-  return <div>RemoverTarefa</div>;
+  function handleAbrirModal(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <span>
+      <Button
+        variant="danger"
+        className="btn btn-sm"
+        onClick={handleAbrirModal}
+        data-testid="btn-abrir-modal"
+      >
+        <FontAwesomeIcon icon={faTrashAlt} />
+      </Button>
+    </span>
+  );
 }
 
 RemoverTarefa.propTypes = {
