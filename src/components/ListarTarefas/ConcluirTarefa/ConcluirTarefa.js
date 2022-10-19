30c577ec7148bbe2079b960310b8420e8e33e68c
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-19 16:05:56 -0300

    Criando o botão de conclusão de tarefa

--- src/components/ListarTarefas/ConcluirTarefa/ConcluirTarefa.js
+++ src/components/ListarTarefas/ConcluirTarefa/ConcluirTarefa.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Modal } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardCheck } from '@fortawesome/free-solid-svg-icons';
 
 function ConcluirTarefa(props) {
-  return <div>ConcluirTarefa</div>;
+  function handleAbrirModal(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <span className={props.className}>
+      <Button
+        className="btn btn-sm"
+        onClick={handleAbrirModal}
+        data-testid="btn-abrir-modal"
+      >
+        <FontAwesomeIcon icon={faClipboardCheck} />
+      </Button>
+    </span>
+  );
 }
 
 ConcluirTarefa.propTypes = {
