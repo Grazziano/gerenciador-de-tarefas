9ef280c69271b6cfe7dc7b83d12f1fa7eff9f009
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-18 23:16:23 -0300

    Criando o html do componente itens das tarefas

--- src/components/ListarTarefas/ItensListaTarefas/ItensListaTarefas.js
+++ src/components/ListarTarefas/ItensListaTarefas/ItensListaTarefas.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 function ItensListaTarefas(props) {
-  return <div>ItensListaTarefas</div>;
+  function marcarConcluida(tarefa) {
+    return tarefa.finish ? 'line-through' : 'none';
+  }
+
+  return (
+    <>
+      {props.tarefas.map((tarefa) => (
+        <tr key={tarefa.id} data-testid="tarefa">
+          <td
+            width="75%"
+            data-testid="nome-tarefa"
+            style={{ textDecoration: marcarConcluida(tarefa) }}
+          >
+            {tarefa.name}
+          </td>
+          <td className="text-right">
+            <Link
+              to={'/atualizar/' + tarefa.id}
+              className={tarefa.finish ? 'hidden' : 'btn btn-warning btn-sm'}
+            >
+              <FontAwesomeIcon icon={faEdit} />
+            </Link>
+          </td>
+        </tr>
+      ))}
+    </>
+  );
 }
 
 ItensListaTarefas.propTypes = {
