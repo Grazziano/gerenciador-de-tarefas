53dedd5ef311fea88d44a70af3b1a53ccadeee8d
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-19 18:56:51 -0300

    Criando modal para remover tarefa

--- src/components/ListarTarefas/RemoverTarefa/RemoverTarefa.js
+++ src/components/ListarTarefas/RemoverTarefa/RemoverTarefa.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 
 function RemoverTarefa(props) {
+  const [exibirModal, setExibirModal] = useState(false);
+
   function handleAbrirModal(event) {
     event.preventDefault();
+    setExibirModal(true);
   }
+
+  function handleFecharModal() {
+    setExibirModal(false);
+  }
+
+  function handleRemoverTarefa() {}
 
   return (
     <span>
@@ -19,6 +28,28 @@ function RemoverTarefa(props) {
       >
         <FontAwesomeIcon icon={faTrashAlt} />
       </Button>
+      <Modal show={exibirModal} onHide={handleFecharModal} data-testid="modal">
+        <Modal.Header closeButton>
+          <Modal.Title>Remover Tarefa</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Deseja realmente remover a seguinte tarefa?
+          <br />
+          <strong>{props.tarefa.name}</strong>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="primary"
+            onClick={handleRemoverTarefa}
+            data-testid="btn-remover"
+          >
+            Sim
+          </Button>
+          <Button variant="light" onClick={handleFecharModal}>
+            Não
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </span>
   );
 }
