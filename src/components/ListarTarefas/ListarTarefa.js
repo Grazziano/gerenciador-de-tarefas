09c3544f1794497ebd2d4af7ebcb834909cc74bb
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-10-21 17:12:08 -0300

    Criando testes unitários de filtro de tarefas

--- src/components/ListarTarefas/ListarTarefa.js
+++ src/components/ListarTarefas/ListarTarefa.js
@@ -53,7 +53,7 @@ function ListarTarefas() {
       obterTarefas();
       setCarregarTarefas(false);
     }
-  }, [carregarTarefas, paginaAtual, ordenarAsc, ordenarDesc]);
+  }, [carregarTarefas, paginaAtual, ordenarAsc, ordenarDesc, filtroTarefa]);
 
   function handleMudarPagina(pagina) {
     setPaginaAtual(pagina);
--- src/components/ListarTarefas/ListarTarefas.test.js
+++ src/components/ListarTarefas/ListarTarefas.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
 import ListarTarefas from './ListarTarefa';
+import Tarefa from '../../models/Tarefa.model';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Teste do componente de listagem de tarefas', () => {
+  const nomePrimeiraTarefa = 'Estudar Javascript';
+  const nomeSegundaTarefa = 'Estudar ReactJS';
+  const nomeTerceiraTarefa = 'Estudar NodeJS';
+
+  beforeEach(() => {
+    localStorage['tarefas'] = JSON.stringify([
+      new Tarefa(1, nomePrimeiraTarefa, false),
+      new Tarefa(2, nomeSegundaTarefa, false),
+      new Tarefa(3, nomeTerceiraTarefa, false),
+    ]);
+  });
+
+  afterEach(() => {
+    delete localStorage['tarefas'];
+  });
+
   it('Deve renderizar o componente sem erros', () => {
-    // render(<ListarTarefas />);
+    render(<ListarTarefas />, { wrapper: BrowserRouter });
+  });
+
+  it('Deve exibir uma tabela contendo 3 tarefas', () => {
+    render(<ListarTarefas />, { wrapper: BrowserRouter });
+    const tabela = screen.getByTestId('tabela');
+    expect(tabela).toHaveTextContent(nomePrimeiraTarefa);
+    expect(tabela).toHaveTextContent(nomeSegundaTarefa);
+    expect(tabela).toHaveTextContent(nomeTerceiraTarefa);
+  });
+
+  it('Deve filtrar os dados da tabela de tarefas', () => {
+    render(<ListarTarefas />, { wrapper: BrowserRouter });
+
+    const txtTarefa = screen.getByTestId('txt-tarefa');
+    fireEvent.change(txtTarefa, { target: { value: nomePrimeiraTarefa } });
+
+    const tabela = screen.getByTestId('tabela');
+
+    expect(tabela).toHaveTextContent(nomePrimeiraTarefa);
+    expect(tabela).not.toHaveTextContent(nomeSegundaTarefa);
+    expect(tabela).not.toHaveTextContent(nomeTerceiraTarefa);
   });
 });
